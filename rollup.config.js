9b742714d9e39d8ecbf4c05b3fef4ba974c627d1
Joe Reeve <joseph.reeve@googlemail.com>
2020-02-02 09:20:07 +0100

    :package: preparing for release

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ import { terser } from 'rollup-plugin-terser';
 
 const production = !process.env.ROLLUP_WATCH;
 
-export default {
+export default [{
 	input: 'src/main.js',
 	output: {
 		sourcemap: true,
@@ -51,7 +51,44 @@ export default {
 	watch: {
 		clearScreen: false
 	}
-};
+},{
+	input: 'src/export.js',
+	output: {
+		sourcemap: true,
+		format: 'module',
+		name: 'app',
+		file: 'dist/bundle.js'
+	},
+	plugins: [
+		svelte({
+			// enable run-time checks when not in production
+			dev: !production,
+			// we'll extract any component CSS out into
+			// a separate file — better for performance
+			css: css => {
+				css.write('public/build/bundle.css');
+			}
+		}),
+
+		// If you have external dependencies installed from
+		// npm, you'll most likely need these plugins. In
+		// some cases you'll need additional configuration —
+		// consult the documentation for details:
+		// https://github.com/rollup/plugins/tree/master/packages/commonjs
+		resolve({
+			browser: true,
+			dedupe: ['svelte']
+		}),
+		commonjs(),
+
+		// If we're building for production (npm run build
+		// instead of npm run dev), minify
+		production && terser()
+	],
+	watch: {
+		clearScreen: false
+	}
+}];
 
 function serve() {
 	let started = false;
